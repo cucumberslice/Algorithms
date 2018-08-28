fbec45c2be2c232e49b135169c7757b30059e88c
Yeshi Wangdi <ywblackberry@gmail.com>
2018-08-28 17:43:14 -0400

    basic linkedList

---
+++ Linked List/basicLinkedList.js
@@ -0,0 +1,32 @@
+// define a node class and a linkedList class
+
+class Node{
+    constructor(data,next=null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null;
+    }
+
+    // add a new node in the list
+    insertFirst(val) {
+        this.head = new Node(val, this.head)
+    }
+
+    //find the number of nodes in the list
+    size() {
+
+        let counter = 0;
+        let node = this.head;
+
+        while(node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
+}
