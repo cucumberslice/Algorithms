92fc3575a4c9938a68211bb1e0afd0a48ca7857f
Yeshi Wangdi <ywblackberry@gmail.com>
2018-09-01 13:32:24 -0400

    plus one linked list

---
+++ Linked List/plusOneLinkedList.js
@@ -0,0 +1,27 @@
+// Plus One Linked List
+//   Go to Discuss
+// Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+// You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+// The digits are stored such that the most significant digit is at the head of the list.
+
+// Example :
+
+// Input: [1,2,3]
+// Output: [1,2,4]
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var plusOne = function(head) {
+    
+};
